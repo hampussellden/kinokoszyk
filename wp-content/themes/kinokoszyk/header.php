d0576d216e0bec8b492e56510a2aa6b174c1f30e
Valisaurus <valley.alosachi@hotmail.com>
2023-03-27 10:07:39 +0200

    header.php
    nothing realy changed

--- wp-content/themes/kinokoszyk/header.php
+++ wp-content/themes/kinokoszyk/header.php
@@ -9,14 +9,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@500&family=DM+Sans:wght@500&family=Oi&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@500&family=DM+Sans:wght@500&family=Oi&display=swap"
+        rel="stylesheet">
 </head>
 
 <body class="bg-stone-900 flex flex-col items-center mx-3 sm:mx-8 h-screen text-slate-50">
     <?php wp_body_open(); ?>
     <?php $menuItems =  wp_get_nav_menu_items('primary-menu'); ?>
 
-    <div class="flex flex-row space-x-4 justify-between w-full border-solid border-b-4 border-slate-50 lg:max-w-screen-md">
+    <div
+        class="flex flex-row space-x-4 justify-between w-full border-solid border-b-4 border-slate-50 lg:max-w-screen-md">
         <h1>logotype</h1>
         <div> hamburger</div>
     </div>
@@ -26,11 +29,12 @@
             <div class="flex">
                 <?php $currentPageId = get_queried_object_id(); ?>
                 <?php if ($menuItems) : ?>
-                    <?php foreach ($menuItems as $item) : ?>
-                        <a title="<?= $item->title ?>" href="<?= $item->url; ?>" class="<?= $currentPageId == $item->object_id ? ' ' : 'hidden'; ?>">
-                            <?= $item->title; ?>
-                        </a>
-                    <?php endforeach ?>
+                <?php foreach ($menuItems as $item) : ?>
+                <a title="<?= $item->title ?>" href="<?= $item->url; ?>"
+                    class="<?= $currentPageId == $item->object_id ? ' ' : 'hidden'; ?>">
+                    <?= $item->title; ?>
+                </a>
+                <?php endforeach ?>
                 <?php endif ?>
             </div>
         </div>
