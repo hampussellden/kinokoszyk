7ffe1f7478222d47bf3d237d3461b964da39cc07
gustavenoksson <gustav.enoksson@live.se>
2023-03-29 16:07:33 +0200

    Changed positioning and styling of blocks
    Updated Explore Movies, Books & Photos blocks.

--- wp-content/themes/kinokoszyk/blocks/explore-photos/block.php
+++ wp-content/themes/kinokoszyk/blocks/explore-photos/block.php
@@ -1,9 +1,9 @@
 <?php
 $image = get_field('image');
 ?>
-<div class="absolute flex flex-col gap-[16px] border-b-4 pb-[31px]">
+<div class="relative flex flex-col gap-[16px] border-b-4 pb-[31px]">
     <h4 class="text-3xl">Explore Photographs</h4>
     <p><?php the_field('description'); ?></p>
     <img src="<?= $image['url']; ?>" alt="<?= $image['alt'] ?>">
-    <button class="w-[138px] h-[48px] bg-red-500 absolute bottom-[31px] right-0">Photographs</button>
+    <button class="absolute w-[138px] h-[48px] bg-kinored text-kinowhite bottom-[31px] right-0">Photographs</button>
 </div>
