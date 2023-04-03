27ad9a0423ece50095320b70d244a81914741abb
hampussellden <hampussellden1@gmail.com>
2023-04-03 11:10:17 +0200

    archive/single books and movies

--- wp-content/themes/kinokoszyk/parts/shared/book.php
+++ wp-content/themes/kinokoszyk/parts/shared/book.php
@@ -1,6 +1,9 @@
-<?= the_content(); ?>
-<a href="<?= the_permalink(); ?>">
-    <button class="bg-red-900">
-        See more >
-    </button>
-</a>
+<article class="pt-12 sm:pt-20 lg:p32 pb-12 sm:pb-32">
+    <h2 class="font-display text-5xl sm:text-8xl lg:text-9xl pb-8 "> <?php the_title(); ?></h2>
+    <div class="flex flex-col gap-x-11 lg:gap-x-20 gap-y-8 sm:flex-row">
+        <div class="sm:max-w-1/2"><?php the_content(); ?></div>
+        <div class="sm:max-w-1/2">
+            <img class="object-contain" src="<?= the_field('image'); ?>" alt="">
+        </div>
+    </div>
+</article>
--- wp-content/themes/kinokoszyk/single-movie.php
+++ wp-content/themes/kinokoszyk/single-movie.php
@@ -1,13 +1,11 @@
 <?php get_header(); ?>
-
-<?php if (have_posts()) : ?>
-    <!-- Product Grid -->
-    <div class="grid grid-cols-3 gap-4 p-8 max-w-7xl mx-auto">
+<main class="flex flex-col mx-5 sm:mx-10 lg:mx-20">
+    <?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
+    <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post() ?>
             <?php get_template_part('parts/shared/movie') ?>
         <?php endwhile; ?>
-        <!-- EOF: Product Grid -->
-    </div>
-<?php endif; ?>
-
+    <?php endif; ?>
+</main>
+<?php get_template_part('parts/shared/letstalk') ?>
 <?php get_footer(); ?>
