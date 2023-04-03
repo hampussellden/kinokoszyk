05037c61cb33d7b0224bf9cba86b7d14841cebed
DouglasLindahl <douglas.lindahl@gmail.com>
2023-04-03 13:54:10 +0200

    fixed css

--- wp-content/themes/kinokoszyk/parts/shared/letstalk.php
+++ wp-content/themes/kinokoszyk/parts/shared/letstalk.php
@@ -7,14 +7,15 @@
         </div>
         <div class="flex gap-7 md:gap-10 lg:gap-20">
             <a href="home" class="text-kinored">
-                <button class="rounded-sm box-content text-sm px-5 py-3 border-solid box-border outline-none md:text-2xl md:py-1 md:px-8" style="box-shadow: inset 0 0 0 2px #751010;">
+                <button class="rounded-sm box-content text-sm px-5 py-3 border-solid box-border outline-none md:text-2xl md:py-1 md:px-8 hover:bg-kinowhite hover:text-kinored" style="box-shadow: inset 0 0 0 2px #751010;">
                     Articles
                 </button>
             </a>
             <a href="home" class="text-kinowhite">
-                <button class="flex gap-x-4 justify-center items-center text-kinowhite rounded-sm text-sm pl-5 pr-7 py-3 bg-kinored md:py-1 md:px-8 md:text-2xl">Contact us
-                    <svg class="md:hidden" width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" fill="white" />
+                <button class="group flex gap-x-4 justify-center items-center text-kinowhite rounded-sm text-sm pl-5 pr-7 py-3 bg-kinored md:py-1 md:px-8 md:text-2xl hover:bg-kinowhite hover:text-kinored">
+                    Contact us
+                    <svg class="fill-current h-4 md:hidden" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
                     </svg>
                 </button>
             </a>
