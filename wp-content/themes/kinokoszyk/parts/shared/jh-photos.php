49416e37a95ab333ba5fea213f20562785065824
hampussellden <hampussellden1@gmail.com>
2023-04-03 16:28:31 +0200

    JH photos first iteration

---
+++ wp-content/themes/kinokoszyk/parts/shared/jh-photos.php
@@ -0,0 +1,3 @@
+<div class="flex justify-center items-center max-h-64 sm:max-h-[692px] lg:max-h-752 max-w-1/2 lg:max-w-1/3 object-contain overflow-hidden">
+    <img class="aspect-auto" src="<?php the_field('image'); ?>" alt="">
+</div>
--- wp-content/themes/kinokoszyk/templates/tamplate-jh-photos.php
+++ wp-content/themes/kinokoszyk/templates/tamplate-jh-photos.php
@@ -1,19 +1,43 @@
 <?php /* Template Name: Photos Template */ ?>
 
-
+<?php get_header('jh'); ?>
 <?php
-//query
+$args = array(
+    'post_type' => 'photo'
+);
+$query = new WP_Query($args);
+$height = '2000px';
 ?>
 
-
-<?php get_header('jh'); ?>
-
-<?php if ($query->have_posts()) : ?>
-    <!-- Product Grid -->
-    <div class="grid grid-cols-3 gap-4 p-8 max-w-7xl mx-auto">
-        <?php while (have_posts()) : the_post() ?>
-            <?php get_template_part('parts/shared/photos') ?>
-        <?php endwhile; ?>
-        <!-- EOF: Product Grid -->
+<main class="flex flex-col mx-3.5 sm:mx-7 lg:mx-[60px]">
+    <div class="px-1.5 sm:px-3 lg:px-5">
+        <?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
     </div>
-<?php endif; ?>
+    <h2 class="font-display text-5xl sm:text-8xl lg:text-9xl lg:pb-10 pt-8 sm:pt-20">Photos</h2>
+    <?php if ($query->have_posts()) : ?>
+        <div class="flex flex-col flex-wrap flex-2 lg:flex-3 gap-y-4 gap-x-4 pt-10 sm:pt-20 max-h-[<?= $height ?>]">
+            <?php while ($query->have_posts()) : $query->the_post() ?>
+                <?php if (get_field('joanna_helander')) :  ?>
+                    <?php get_template_part('parts/shared/jh-photos') ?>
+                <?php endif ?>
+            <?php endwhile; ?>
+        </div>
+    <?php endif; ?>
+</main>
+<!-- test -->
+<main class="flex flex-col mx-3.5 sm:mx-7 lg:mx-[60px]">
+    <div class="px-1.5 sm:px-3 lg:px-5">
+        <?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
+    </div>
+    <h2 class="font-display text-5xl sm:text-8xl lg:text-9xl lg:pb-10 pt-8 sm:pt-20">Photos</h2>
+    <?php if ($query->have_posts()) : ?>
+        <div class="grid gridgap-y-4 gap-x-4 pt-10 sm:pt-20 max-h-[1500px]">
+            <?php while ($query->have_posts()) : $query->the_post() ?>
+                <?php if (get_field('joanna_helander')) :  ?>
+                    <?php get_template_part('parts/shared/jh-photos') ?>
+                <?php endif ?>
+            <?php endwhile; ?>
+        </div>
+    <?php endif; ?>
+</main>
+<?php get_footer(); ?>
