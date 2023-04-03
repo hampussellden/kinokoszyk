2944a55311d98b59673478f903f0c1ac9c7f1aa3
hampussellden <hampussellden1@gmail.com>
2023-04-03 16:27:28 +0200

    movie parts button component add

--- wp-content/themes/kinokoszyk/parts/shared/movies.php
+++ wp-content/themes/kinokoszyk/parts/shared/movies.php
@@ -5,12 +5,8 @@
             <p class="font-body sm:text-lg lg:text-xl line-clamp-4"><?php the_field('description'); ?></p>
         </div>
         <a href="<?php the_permalink(); ?>">
-            <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit sm:static sm:pl-[32px] sm:pr-[32px] sm:pt-[13.9px] sm:pb-[13.9px] sm:gap-[24px] sm:text-[24px] cursor-pointer">See more
-                <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" fill="white" />
-                </svg>
-            </button>
+            <?php get_template_part('parts/shared/seemore-btn') ?>
         </a>
     </div>
-    <img class="w-[91.64%] max-h-[290px] sm:max-h-[350px] sm:min-w-[52%] object-cover object-top" src="<?php the_field('image'); ?>" alt="">
+    <img class="w-[91.64%] sm:min-w-[52%] object-cover object-top" src="<?php the_field('image'); ?>" alt="">
 </article>
