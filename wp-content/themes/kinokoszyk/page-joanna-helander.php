ac10a2a270bd8cd4833e3e4ac9483c68ff54d932
DouglasLindahl <douglas.lindahl@gmail.com>
2023-04-04 10:20:58 +0200

    Update page-joanna-helander.php

--- wp-content/themes/kinokoszyk/page-joanna-helander.php
+++ wp-content/themes/kinokoszyk/page-joanna-helander.php
@@ -2,4 +2,14 @@
 
 <?php get_header('jh'); ?>
 
+<section class="absolute top-0 w-screen h-screen bg-kinoblack">
+    <section class="h-screen flex justify-start items-end">
+        <section>
+            <h1 class="font-display text-7xl w-1/2 md:w-full">JOANNA HELANDER</h1>
+            <h2 class="font-display text-7xl hidden md:block">KINOKOSZYK</h2>
+        </section>
+    </section>
+</section>
+
+
 <?php get_footer(); ?>
