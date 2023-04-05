06c0b1ab3f60dec7ea1fd8da27a073b786624274
hampussellden <hampussellden1@gmail.com>
2023-04-05 12:56:07 +0200

    JH photos pages

--- wp-content/themes/kinokoszyk/templates/tamplate-jh-photos.php
+++ wp-content/themes/kinokoszyk/templates/tamplate-jh-photos.php
@@ -3,10 +3,15 @@
 <?php get_header('jh'); ?>
 <?php
 $args = array(
-    'post_type' => 'photo'
+    'post_type' => 'photo',
+    'meta_key' => 'joanna_helander',
+    'meta_value' => true,
 );
 $query = new WP_Query($args);
-$height = '2000px';
+$total = $query->found_posts;
+$aThird = $total / 3;
+
+$height = '1500px';
 ?>
 
 <main class="flex flex-col mx-3.5 sm:mx-7 lg:mx-[60px]">
@@ -15,23 +20,7 @@ $height = '2000px';
     </div>
     <h2 class="font-display text-5xl sm:text-8xl lg:text-9xl lg:pb-10 pt-8 sm:pt-20">Photos</h2>
     <?php if ($query->have_posts()) : ?>
-        <div class="flex flex-col flex-wrap flex-2 lg:flex-3 gap-y-4 gap-x-4 pt-10 sm:pt-20 max-h-[<?= $height ?>]">
-            <?php while ($query->have_posts()) : $query->the_post() ?>
-                <?php if (get_field('joanna_helander')) :  ?>
-                    <?php get_template_part('parts/shared/jh-photos') ?>
-                <?php endif ?>
-            <?php endwhile; ?>
-        </div>
-    <?php endif; ?>
-</main>
-<!-- test -->
-<main class="flex flex-col mx-3.5 sm:mx-7 lg:mx-[60px]">
-    <div class="px-1.5 sm:px-3 lg:px-5">
-        <?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
-    </div>
-    <h2 class="font-display text-5xl sm:text-8xl lg:text-9xl lg:pb-10 pt-8 sm:pt-20">Photos</h2>
-    <?php if ($query->have_posts()) : ?>
-        <div class="grid gridgap-y-4 gap-x-4 pt-10 sm:pt-20 max-h-[1500px]">
+        <div class="flex flex-col items-center flex-wrap pt-10 sm:pt-20 max-h-[1500px] overflow-hidden">
             <?php while ($query->have_posts()) : $query->the_post() ?>
                 <?php if (get_field('joanna_helander')) :  ?>
                     <?php get_template_part('parts/shared/jh-photos') ?>
