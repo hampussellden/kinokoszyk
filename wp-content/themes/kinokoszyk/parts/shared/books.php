1d140171c114c0222ee3ba93b1459f5e293dd527
hampussellden <hampussellden1@gmail.com>
2023-04-06 15:51:31 +0200

    added JH books header img to lib

--- wp-content/themes/kinokoszyk/parts/shared/books.php
+++ wp-content/themes/kinokoszyk/parts/shared/books.php
@@ -6,7 +6,7 @@ $alt = $image['alt'];
 
 <a href="<?= the_permalink(); ?>" class="flex flex-col justify-start items-start py-8 border-b-2 border-solid border-kinowhite max-w-50% sm:max-w-[300px] lg:max-w-[400px] px-1.5 sm:px-3 lg:px-5">
     <?php if (get_field('image')) : ?>
-        <img class="object-cover aspect-[2/2.5]" alt="<?= $alt; ?>" srcset="<?= $srcset; ?>" sizes="33vw" />
+        <img class="object-cover aspect-[2/2.5]" alt="<?= $alt; ?>" srcset="<?= $srcset; ?>" sizes="33vw" loading="lazy" />
     <?php endif; ?>
     <div class="flex flex-col gap-1">
         <h3 class="font-display text-2xl sm:text-4xl lg:text-5xl"><?= the_title(); ?></h3>
