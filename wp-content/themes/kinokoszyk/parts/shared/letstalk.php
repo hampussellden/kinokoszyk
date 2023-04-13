36a43ab5891d6a7a708ef1781ccff1f9991dcfc5
DouglasLindahl <douglas.lindahl@gmail.com>
2023-04-13 13:49:25 +0200

    updated y-padding on buttons in lets talk

--- wp-content/themes/kinokoszyk/parts/shared/letstalk.php
+++ wp-content/themes/kinokoszyk/parts/shared/letstalk.php
@@ -7,12 +7,12 @@
         </div>
         <div class="flex gap-7 md:gap-10 lg:gap-20">
             <a href="news-2" class="text-kinored">
-                <button class="rounded-sm box-content text-sm px-5 py-3 border-solid box-border outline-none md:text-2xl md:py-1 md:px-8 hover:bg-kinowhite hover:text-kinored" style="box-shadow: inset 0 0 0 2px #751010;">
+                <button class="rounded-sm box-content text-sm px-5 py-3 border-solid box-border outline-none md:text-2xl md:px-8 hover:bg-kinowhite hover:text-kinored" style="box-shadow: inset 0 0 0 2px #751010;">
                     Articles
                 </button>
             </a>
             <a href="contact" class="text-kinowhite">
-                <button class="group flex gap-x-4 justify-center items-center text-kinowhite rounded-sm text-sm pl-5 pr-7 py-3 bg-kinored md:py-1 md:px-8 md:text-2xl hover:bg-kinowhite hover:text-kinored">
+                <button class="group flex gap-x-4 justify-center items-center text-kinowhite rounded-sm text-sm pl-5 pr-7 bg-kinored py-3 md:px-8 md:text-2xl hover:bg-kinowhite hover:text-kinored">
                     Contact us
                     <svg class="fill-current h-4 md:hidden" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
