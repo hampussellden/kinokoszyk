86af50979f66d0c2198ee221b5ce1d7f01fa71d9
gustavenoksson <gustav.enoksson@live.se>
2023-04-13 11:29:51 +0200

    added collaboration part

--- wp-content/themes/kinokoszyk/front-page.php
+++ wp-content/themes/kinokoszyk/front-page.php
@@ -65,77 +65,78 @@ if ($queryHero->have_posts()) : ?>
                 </p>
                 <img class="max-w-1/2 object-contain hidden self-start sm:inline-block" alt="<?= $altAbout; ?>" srcset="<?= $srcsetAbout; ?>" sizes="50vw" loading="lazy" />
             </div>
-
-            <section>
-                <article class="relative flex flex-col gap-[18px] border-b pb-[31px] pt-[48px] h-auto w-auto lg:flex-row lg:justify-center lg:items-center lg:gap-11 lg:border-t lg:pt-[60px] lg:pb-[89px] lg:pt-[80px]">
-                    <div class="lg:flex lg:flex-col lg:h-[391px] lg:w-full lg:justify-center lg:gap-10">
-                        <div class="flex flex-col gap-4 lg:gap-5">
-                            <h3 class="font-display text-3xl"><?= the_field("explore_movies_title") ?></h3>
-                            <p class="max-w-[70ch]"><?php the_field("explore_movies_description"); ?></p>
-                        </div>
-                        <a href="movie">
-                            <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit lg:static lg:pl-[32px] lg:pr-[32px] lg:pt-[13.9px] lg:pb-[13.9px] lg:gap-[24px] lg:text-[24px] hover:bg-kinowhite hover:text-kinored">Movies
-                                <svg class="fill-current h-4" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
-                                </svg>
-                            </button>
-                        </a>
-                    </div>
-                    <?php if ($imageMovies) : ?>
-                        <div class="max-w-[91.64%] lg:max-w-1/2">
-                            <img alt="<?= $altMovies; ?>" srcset="<?= $srcsetMovies; ?>" sizes="100vw" loading="lazy" />
-                        </div>
-                    <?php endif; ?>
-                </article>
-
-                <article class="relative flex flex-col gap-[18px] border-b pb-[31px] pt-[48px] h-auto w-auto lg:flex-row lg:justify-center lg:items-center lg:gap-11 lg:border-t lg:pt-[60px] lg:pb-[89px] lg:pt-[80px]">
-                    <div class="lg:flex lg:flex-col lg:h-[391px] lg:w-full lg:justify-center lg:gap-10">
-                        <div class="flex flex-col gap-4 lg:gap-5">
-                            <h3 class="font-display text-3xl"><?= the_field("explore_books_title") ?></h3>
-                            <p class="max-w-[70ch]"><?php the_field("explore_books_description"); ?></p>
-                        </div>
-                        <a href="book">
-                            <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit lg:static lg:pl-[32px] lg:pr-[32px] lg:pt-[13.9px] lg:pb-[13.9px] lg:gap-[24px] lg:text-[24px] hover:bg-kinowhite hover:text-kinored">Books
-                                <svg class="fill-current h-4" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
-                                </svg>
-                            </button>
-                        </a>
-                    </div>
-                    <?php if ($imageBooks) : ?>
-                        <div class="max-w-[91.64%] lg:max-w-1/2">
-                            <img alt="<?= $altBooks; ?>" srcset="<?= $srcsetBooks; ?>" sizes="100vw" loading="lazy" />
-                        </div>
-                    <?php endif; ?>
-                </article>
-
-                <article class="relative flex flex-col gap-[18px] border-b pb-[31px] pt-[48px] h-auto w-auto lg:flex-row lg:justify-center lg:items-center lg:gap-11 lg:border-t lg:pt-[60px] lg:pb-[89px] lg:pt-[80px]">
-                    <div class="lg:flex lg:flex-col lg:h-[391px] lg:w-full lg:justify-center lg:gap-10">
-                        <div class="flex flex-col gap-4 lg:gap-5">
-                            <h3 class="font-display text-3xl"><?= the_field("explore_photographs_title") ?></h3>
-                            <p class="max-w-[70ch]"><?php the_field("explore_photographs_description"); ?></p>
-                        </div>
-                        <a href="photo">
-                            <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit lg:static lg:pl-[32px] lg:pr-[32px] lg:pt-[13.9px] lg:pb-[13.9px] lg:gap-[24px] lg:text-[24px] hover:bg-kinowhite hover:text-kinored">Photographs
-                                <svg class="fill-current h-4" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
-                                </svg>
-                            </button>
-                        </a>
-                    </div>
-                    <?php if ($imagePhotographs) : ?>
-                        <div class="max-w-[91.64%] lg:max-w-1/2">
-                            <img alt="<?= $altPhotographs; ?>" srcset="<?= $srcsetPhotographs; ?>" sizes="100vw" loading="lazy" />
-                        </div>
-                    <?php endif; ?>
-                </article>
-            </section>
-        <?php endwhile; ?>
-    <?php endif; ?>
-
         </main>
 
-        <?php get_template_part('parts/shared/letstalk'); ?>
+        <?php get_template_part('parts/shared/collaborations'); ?>
+
+        <section>
+            <article class="relative flex flex-col gap-[18px] border-b pb-[31px] pt-[48px] h-auto w-auto lg:flex-row lg:justify-center lg:items-center lg:gap-11 lg:border-t lg:pt-[60px] lg:pb-[89px] lg:pt-[80px]">
+                <div class="lg:flex lg:flex-col lg:h-[391px] lg:w-full lg:justify-center lg:gap-10">
+                    <div class="flex flex-col gap-4 lg:gap-5">
+                        <h3 class="font-display text-3xl"><?= the_field("explore_movies_title") ?></h3>
+                        <p class="max-w-[70ch]"><?php the_field("explore_movies_description"); ?></p>
+                    </div>
+                    <a href="movie">
+                        <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit lg:static lg:pl-[32px] lg:pr-[32px] lg:pt-[13.9px] lg:pb-[13.9px] lg:gap-[24px] lg:text-[24px] hover:bg-kinowhite hover:text-kinored">Movies
+                            <svg class="fill-current h-4" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
+                            </svg>
+                        </button>
+                    </a>
+                </div>
+                <?php if ($imageMovies) : ?>
+                    <div class="max-w-[91.64%] lg:max-w-1/2">
+                        <img alt="<?= $altMovies; ?>" srcset="<?= $srcsetMovies; ?>" sizes="100vw" loading="lazy" />
+                    </div>
+                <?php endif; ?>
+            </article>
+
+            <article class="relative flex flex-col gap-[18px] border-b pb-[31px] pt-[48px] h-auto w-auto lg:flex-row lg:justify-center lg:items-center lg:gap-11 lg:border-t lg:pt-[60px] lg:pb-[89px] lg:pt-[80px]">
+                <div class="lg:flex lg:flex-col lg:h-[391px] lg:w-full lg:justify-center lg:gap-10">
+                    <div class="flex flex-col gap-4 lg:gap-5">
+                        <h3 class="font-display text-3xl"><?= the_field("explore_books_title") ?></h3>
+                        <p class="max-w-[70ch]"><?php the_field("explore_books_description"); ?></p>
+                    </div>
+                    <a href="book">
+                        <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit lg:static lg:pl-[32px] lg:pr-[32px] lg:pt-[13.9px] lg:pb-[13.9px] lg:gap-[24px] lg:text-[24px] hover:bg-kinowhite hover:text-kinored">Books
+                            <svg class="fill-current h-4" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
+                            </svg>
+                        </button>
+                    </a>
+                </div>
+                <?php if ($imageBooks) : ?>
+                    <div class="max-w-[91.64%] lg:max-w-1/2">
+                        <img alt="<?= $altBooks; ?>" srcset="<?= $srcsetBooks; ?>" sizes="100vw" loading="lazy" />
+                    </div>
+                <?php endif; ?>
+            </article>
+
+            <article class="relative flex flex-col gap-[18px] border-b pb-[31px] pt-[48px] h-auto w-auto lg:flex-row lg:justify-center lg:items-center lg:gap-11 lg:border-t lg:pt-[60px] lg:pb-[89px] lg:pt-[80px]">
+                <div class="lg:flex lg:flex-col lg:h-[391px] lg:w-full lg:justify-center lg:gap-10">
+                    <div class="flex flex-col gap-4 lg:gap-5">
+                        <h3 class="font-display text-3xl"><?= the_field("explore_photographs_title") ?></h3>
+                        <p class="max-w-[70ch]"><?php the_field("explore_photographs_description"); ?></p>
+                    </div>
+                    <a href="photo">
+                        <button class="absolute flex flex-row justify-center items-center gap-[8px] p-[12px] bg-kinored text-kinowhite bottom-[31px] right-0 text-xl w-fit h-fit lg:static lg:pl-[32px] lg:pr-[32px] lg:pt-[13.9px] lg:pb-[13.9px] lg:gap-[24px] lg:text-[24px] hover:bg-kinowhite hover:text-kinored">Photographs
+                            <svg class="fill-current h-4" viewBox="0 0 9 16" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.9981 7.99966L8.99931 7.99847L7.63578 6.65891L7.63456 6.6601L1.36422 0.5L0.000689844 1.83956L6.27103 7.99966L0 14.1604L1.36353 15.5L7.63456 9.33922L7.63647 9.34109L9 8.00153L8.9981 7.99966Z" />
+                            </svg>
+                        </button>
+                    </a>
+                </div>
+                <?php if ($imagePhotographs) : ?>
+                    <div class="max-w-[91.64%] lg:max-w-1/2">
+                        <img alt="<?= $altPhotographs; ?>" srcset="<?= $srcsetPhotographs; ?>" sizes="100vw" loading="lazy" />
+                    </div>
+                <?php endif; ?>
+            </article>
+        </section>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_template_part('parts/shared/letstalk'); ?>
 
 
-        <?php get_footer(); ?>
+<?php get_footer(); ?>
